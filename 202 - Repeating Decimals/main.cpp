4496c3c6c1b151a04ea4b4174d1a962283176779
MD ALIF HOSSAIN KARUZ <karuzm121@gmail.com>
2023-11-23 05:49:34 +0600

    Almost Done

--- 202 - Repeating Decimals/main.cpp
+++ 202 - Repeating Decimals/main.cpp
@@ -2,5 +2,39 @@
 using namespace std;
 int main()
 {
-    printf("Hello");
+    vector<int> result;
+    vector<int> remainder;
+    int a = 397;
+    int b = 1;
+    int res, rem;
+    int i = 0, k = 0;
+    int end = 0;
+    rem = b % a;
+    while (end == 0)
+    {
+        b = rem * 10;
+        res = b / a;
+        rem = b % a;
+
+        result.push_back(res);
+        remainder.push_back(b);
+
+        if (i > 0)
+        {
+            for (k = 0; k < i; k++)
+            {
+                if (remainder[k] == remainder[i])
+                {
+                    end = 1;
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+    for (k; k < i - 1; k++)
+    {
+        cout << result[k] << " ";
+    }
+    return 0;
 }
