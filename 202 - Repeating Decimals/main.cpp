dfbc6a023f81a9a6373835dc206addb0514e2f99
MD ALIF HOSSAIN KARUZ <karuzm121@gmail.com>
2023-11-24 03:29:38 +0600

    Done

--- 202 - Repeating Decimals/main.cpp
+++ 202 - Repeating Decimals/main.cpp
@@ -2,39 +2,65 @@
 using namespace std;
 int main()
 {
-    vector<int> result;
-    vector<int> remainder;
-    int a = 397;
-    int b = 1;
-    int res, rem;
-    int i = 0, k = 0;
-    int end = 0;
-    rem = b % a;
-    while (end == 0)
+    int a, b;
+    while (scanf("%d %d", &b, &a) != EOF)
     {
-        b = rem * 10;
-        res = b / a;
+        vector<int> result;
+        vector<int> remainder;
+
+        int x = b;
+        int res = 0, rem = 0;
+        int i = 0, k = 0, j = 0, count = 0;
+        int cycleLength = 0;
+        int end = 0;
         rem = b % a;
-
-        result.push_back(res);
-        remainder.push_back(b);
-
-        if (i > 0)
+        while (end == 0)
         {
-            for (k = 0; k < i; k++)
+            b = rem * 10;
+            res = b / a;
+            rem = b % a;
+
+            result.push_back(res);
+            remainder.push_back(b);
+
+            if (i > 0)
             {
-                if (remainder[k] == remainder[i])
+                for (k = 0; k < i; k++)
                 {
-                    end = 1;
-                    break;
+                    if (remainder[k] == remainder[i])
+                    {
+                        end = 1;
+                        break;
+                    }
                 }
             }
+            i++;
         }
-        i++;
-    }
-    for (k; k < i - 1; k++)
-    {
-        cout << result[k] << " ";
+        cycleLength = i - k - 1;
+
+        cout << x << "/" << a << " = " << x / a << ".";
+        for (j = 0; j < k; j++)
+        {
+            cout << result[j];
+        }
+        cout << "(";
+        if (cycleLength > 50)
+        {
+            for (k; k < 50; k++)
+            {
+                cout << result[k];
+            }
+            cout << "...";
+        }
+        else
+        {
+            for (k; k < i - 1; k++)
+            {
+                cout << result[k];
+            }
+        }
+        cout << ")" << endl;
+        cout << "   " << cycleLength << " = number of digits in repeating cycle\n\n";
     }
     return 0;
 }
